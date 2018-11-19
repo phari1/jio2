f66149a0602995b6f2cae219139ec05df0ace2e7
srihari <panniru.hari@gmail.com>
2018-11-19 15:24:56 +0530

    new multideploy.sh

--- multi_deploy.sh
+++ multi_deploy.sh
@@ -6,10 +6,10 @@ if [ $ENVIRONMENT = "QA" ];then
 #
 	echo "Deploying war file into $i QA server.."
 	sleep 3
-	sshpass -p "gamut" scp target/gamutkart.war gamut@$i:/home/gamut/Distros/apache-tomcat-8.5.23/webapps
+	sshpass -p "santhu" scp target/jio.war santhu@$i:/home/santhu/soft/apache-tomcat-9.0.12/webapps
 #
 	echo "Starting tomcat server in $i QA server.."
-	sshpass -p "gamut" ssh gamut@$i "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.23/bin/startup.sh"
+	sshpass -p "gamut" ssh santhu@$i "JAVA_HOME=/home/santhu/soft/jdk-10.0.2" "santhu/soft/apache-tomcat-9.0.12/bin/startup.sh"
 #
 	done
 	echo "Deployment is succussful!"
